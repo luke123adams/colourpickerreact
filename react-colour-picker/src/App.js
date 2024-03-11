0837f6e82e938b9342b075be2807acb77835df43
luke123adams <lxa207@gmail.com>
2024-03-11 14:49:21 +0000

    installed dependencies

---
+++ react-colour-picker/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import styled from "styled-components"
+import './App.css';
+
+function App() {
+  return (
+    <AppStyled>
+
+    </AppStyled>
+  );
+}
+
+const AppStyled = styled.div`
+    min-height: 100vh;
+  background-color: slateblue
+`;
+
+export default App;
---
+++ react-colour-picker/src/GlobalStyle.js
@@ -0,0 +1,20 @@
+import {createGlobalStyle} from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+}
+body{
+    font-size: 1.2rem;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: #0F0F0F;
+}
+a{
+    text-decoration: none;
+    color: #fff;
+}
+`;
