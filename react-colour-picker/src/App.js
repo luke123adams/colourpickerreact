ea938a1380342f5268808f10f8df8aa97f0e3a16
luke123adams <lxa207@gmail.com>
2024-03-12 13:22:29 +0000

    added and styled palettes

--- react-colour-picker/src/App.js
+++ react-colour-picker/src/App.js
@@ -1,5 +1,4 @@
 import styled from "styled-components"
-import './App.css';
 import bg from './bg.png'
 import bg2 from './grid.png'
 import Palettes from "./Components/Palettes"
--- react-colour-picker/src/Components/Palettes.js
+++ react-colour-picker/src/Components/Palettes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { palette } from './myPalettes'
+import styled from 'styled-components'
 
 export default function Palettes() {
 
@@ -8,6 +9,43 @@ export default function Palettes() {
 
     console.log(myPalettes)
   return (
-    <div>Palettes</div>
+    <PalettesStyled>
+        <div className='palettes'>
+        {
+            myPalettes.map((pal, index)=>{
+                console.log(pal)
+                return <div className='palette'>
+                    {pal.colors.map((col, i)=>{
+                        return <div key={i} className='color'
+                        style={{backgroundColor: col}}
+                        >
+
+                        </div>
+                    })}
+                </div>
+            })
+        }            
+        </div>       
+    </PalettesStyled>
   )
 }
+
+const PalettesStyled = styled.div`
+    .palettes{
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+        grid-gap: 25px;
+        padding: 2rem 18rem;
+        transition: all .3s ease;
+        .palette{
+            display: grid;
+            grid-template-columns: repeat(5, 1fr);
+            width: 100%;
+            height: 250px;
+            .color{
+                width: 100%;
+                height: 100%;
+            }
+        }
+    }
+`;
