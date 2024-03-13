365f0f6e862eae7d6fe1ea0643e1dd3c09bb6020
luke123adams <lxa207@gmail.com>
2024-03-13 13:35:17 +0000

    added delete color and rgb toggle functions

--- react-colour-picker/src/Components/Palette.js
+++ react-colour-picker/src/Components/Palette.js
@@ -15,8 +15,34 @@ export default function Palette() {
     // state
 
     const [myPalette, setMyPalette] = React.useState(initialPalette)
+    const [toRgb, setToRgb] = React.useState('hex')
 
-    console.log(myPalette)
+    const toggleToRgb = (e) => {
+        setToRgb(e.target.value)
+    }
+
+    const deleteColor = (index) => {
+        const newColors = [...myPalette.colors]
+        newColors.splice(index, 1)
+
+        setMyPalette({...myPalette, colors: newColors})
+        
+
+
+
+    }
+
+    const clear = (index) => {
+
+    }
+
+    const createColor = () => {
+
+    }
+
+    const handleColorChange = (color) => {
+
+    }
 
   return (
     <PaletteStyled>
@@ -25,7 +51,7 @@ export default function Palette() {
             <Link to={'/'}>&larr;&nbsp; Back</Link>
         </div>
         <div className="select-type">
-            <select >
+            <select defaultValue="hex" onChange={(e)=>{toggleToRgb(e)}}>
                 <option value="hex">HEX</option>
                 <option value="rgb">RGB</option>
             </select>
@@ -40,7 +66,15 @@ export default function Palette() {
     <div className="colors">
         {myPalette.colors.map((color, index) => {
            return <div key={index}
-            style={{background: color}} className="full-color"></div>
+            style={{background: color}} className="full-color">
+                <h4>
+
+                </h4>
+                <button className="btn-icon" onClick={()=>{
+                    deleteColor(index);
+                }}>{del}</button>
+
+            </div>
         })}
     </div>
     </PaletteStyled>
@@ -51,6 +85,64 @@ const PaletteStyled = styled.div`
     position: relative;
     z-zIndex: 5;
     width: 100%;
+
+    .btn-icon{
+        outline: none;
+        cursor: pointer;
+        font-size: 1.5rem;
+        border: none;
+        outline: none;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: .5rem 1rem;
+        border-radius: 7px;
+        color: white;
+        background: #A855F7;
+        transition: all 0.3s ease-in-out;
+        &:hover{
+            background: #0D0B33;
+        }
+    }
+
+    .header-items{
+        height: 6vh;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0 2rem;
+        background-color: #fff;
+        .link-con{
+            a{
+                text-decoration: none;
+                font-family: inherit;
+                font-size: inherit;
+                color: #000;
+                font-weight: 500;
+                width: 50%;
+            }
+        }
+        select{
+            font-family: inherit;
+            font-size: inherit;
+            font-weight: 500;
+            padding: 0.5rem 1rem;
+            border: none;
+            border-radius: 5px;
+            outline: none;
+            color: #fff;
+            background-color: #000;
+            cursor: pointer;
+        }
+        .right{
+            display: flex;
+            align-items: center;
+            gap: .8rem;
+            button:last-child{
+                background-color: red;
+            }
+        }
+    }
 
     .colors{
         display: grid;
